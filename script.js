1eda94c7ef293a6ffddb7f917fe141e08b80a084
Moroz Tetiana <tania@Morozs-MacBook-Air.local>
2023-03-14 19:04:05 +0200

    Generated Joke with XMLHttpReguest

---
+++ script.js
@@ -0,0 +1,20 @@
+
+const button = document.querySelector('.btn')
+const div = document.querySelector('.joke')
+
+
+function getRequest() {
+    const xhr = new XMLHttpRequest()
+    xhr.open('GET', 'https://api.chucknorris.io/jokes/random')
+    
+
+    xhr.onreadystatechange = function() {
+    if(this.readyState === 4 && this.status === 200) {
+        const data = JSON.parse(this.responseText)
+        div.innerHTML = data.value
+    }
+    }
+    xhr.send()
+}
+
+button.addEventListener('click', getRequest)
